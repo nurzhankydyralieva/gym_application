81bb70eb0897e70ab31171d3408709ef2cb428d9
Nurzhan <nurzhankydyralieva@gmail.com>
2024-01-11 23:30:15 +0600

    added entities

---
+++ src/main/java/com/epam/xstack/models/entity/TrainingType.java
@@ -0,0 +1,23 @@
+package com.epam.xstack.models.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "training_type")
+public class TrainingType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "training_type_id")
+    private Long id;
+    @Column(name = "training_type")
+    private String trainingType;
+
+}
